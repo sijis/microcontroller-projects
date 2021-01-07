0d6086022f3340e7ab4cc12a871e4103772303ae
Sijis Aviles <sijis.aviles+github@gmail.com>
2021-01-06 21:06:35 -0600

    chore: Add titano stock code

---
+++ titano/secrets.py
@@ -0,0 +1,10 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'xxxx',
+    'password' : 'xxxx',
+    'timezone' : "xxxx", # http://worldtimeapi.org/timezones
+    'github_token' : 'xxxx',
+    'hackaday_token' : 'xxxx',
+    }
