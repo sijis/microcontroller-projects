e6662cdfc5c9bd3b2308e7677a051a080ad863f6
Sijis Aviles <sijis.aviles+github@gmail.com>
2021-01-22 21:10:41 -0600

    refactor: Use LED strip

--- battlebots/BattleBots.ino
+++ battlebots/BattleBots.ino
@@ -1,59 +1,85 @@
+#include <FastLED.h>
 #include "pitches.h"
 
-int yellowPin = 5;
-int greenPin = 6;
 int buttonApin = 9;
 int buttonBpin = 10;
 
 //https://forum.arduino.cc/index.php?topic=424068.0
-unsigned long previousMillis = 0;        // will store last time LED was updated
-const unsigned long interval = 15UL*1000UL;   // Change the LED every 12 hours
+unsigned long previousMillis = 0;             // will store last time LED was updated
+const unsigned long interval = 15UL * 1000UL; // Change the LED every 15 seconds
+
+// FastLED
+#define NUM_LEDS 15
+#define DATA_PIN 3
+#define LED_TYPE WS2812B
+#define CLOCK_PIN 13
+
+#define BRIGHTNESS 96
+#define COLOR_ORDER GRB
+
+CRGB leds[NUM_LEDS];
 
 void setup()
 {
-  pinMode(yellowPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(buttonApin, INPUT_PULLUP);
-  pinMode(buttonBpin, INPUT_PULLUP);
-  Serial.begin(9600);
+    pinMode(buttonApin, INPUT_PULLUP);
+    pinMode(buttonBpin, INPUT_PULLUP);
+    Serial.begin(9600);
+
+    FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+    FastLED.setBrightness(BRIGHTNESS);
+    FastLED.clear();
+    FastLED.show();
 }
 
 void loop()
 {
-  //  Serial.println(" Button: " + LOW);
-  if (digitalRead(buttonApin) == LOW)
-  {
-    startBlink();
-  }
+    //  Serial.println(" Button: " + LOW);
+    if (digitalRead(buttonApin) == LOW)
+    {
+        Serial.println("Start Sequence.");
+        startBattleSequence();
+        previousMillis = millis();
+        Serial.println("End Sequence.");
+    }
 
-
-  // turn off led after end
-    unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    // save the time you should have toggled the LED
-    previousMillis += interval;
-    digitalWrite(greenPin, LOW);
-  }
-
-  if (digitalRead(buttonBpin) == LOW)
-  {
-    digitalWrite(yellowPin, LOW);
-    digitalWrite(greenPin, LOW);
-  }
+    endTimerSequence();
 }
 
-void startBlink() {
-  for (int counter = 1; counter <= 3; counter++) {
-    digitalWrite(yellowPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-    tone(8, NOTE_F5, 900);
-    delay(1000);              // wait for a second
-    digitalWrite(yellowPin, LOW);    // turn the LED off by making the voltage LOW
-    delay(200);              // wait for a second
-    Serial.println("Beep.");
-  }
-  delay(200);
-  digitalWrite(greenPin, HIGH);
-  tone(8, NOTE_A5, 900);
-  Serial.println("Beep!!!");
+void startBattleSequence()
+{
+    for (int counter = 1; counter <= 3; counter++)
+    {
+        fill_solid(leds, NUM_LEDS, CRGB::Yellow);
+        tone(8, NOTE_F5, 900);
+        FastLED.delay(1000);
 
+        FastLED.clear();
+        FastLED.delay(200);
+        Serial.println("Beep.");
+    }
+
+    delay(200);
+    fill_solid(leds, NUM_LEDS, CRGB::Green);
+    FastLED.show();
+    tone(8, NOTE_A5, 900);
+    Serial.println("Beep!!!");
+}
+
+void endTimerSequence()
+{
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval)
+    {
+        // save the time you should have toggled the LED
+        previousMillis += interval;
+        Serial.println("Shut off time");
+        FastLED.clear();
+        FastLED.show();
+    }
+
+    if (digitalRead(buttonBpin) == LOW)
+    {
+        FastLED.clear();
+        FastLED.show();
+    }
 }
