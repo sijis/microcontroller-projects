499a50ec2133d91a58d204249013d9db65f99e8c
Sijis Aviles <sijis.aviles+github@gmail.com>
2021-01-23 00:38:18 -0600

    feat: Add light confetti sequence before countdown

--- battlebots/BattleBots.ino
+++ battlebots/BattleBots.ino
@@ -4,7 +4,7 @@
 int buttonApin = 9;
 int buttonBpin = 10;
 
-//https://forum.arduino.cc/index.php?topic=424068.0
+// https://forum.arduino.cc/index.php?topic=424068.0
 unsigned long previousMillis = 0;             // will store last time LED was updated
 const unsigned long interval = 15UL * 1000UL; // Change the LED every 15 seconds
 
@@ -13,11 +13,12 @@ const unsigned long interval = 15UL * 1000UL; // Change the LED every 15 seconds
 #define DATA_PIN 3
 #define LED_TYPE WS2812B
 #define CLOCK_PIN 13
-
+#define FRAMES_PER_SECOND 120
 #define BRIGHTNESS 96
 #define COLOR_ORDER GRB
 
 CRGB leds[NUM_LEDS];
+uint8_t gHue = 0; // rotating "base color"
 
 void setup()
 {
@@ -37,6 +38,7 @@ void loop()
     if (digitalRead(buttonApin) == LOW)
     {
         Serial.println("Start Sequence.");
+        confetti();
         startBattleSequence();
         previousMillis = millis();
         Serial.println("End Sequence.");
@@ -82,4 +84,27 @@ void endTimerSequence()
         FastLED.clear();
         FastLED.show();
     }
+}
+
+void confetti()
+{
+    // random colored speckles that blink in and fade smoothly
+    for (int counter = 1; counter <= 350; counter++)
+    {
+        fadeToBlackBy(leds, NUM_LEDS, 10);
+        int pos = random16(NUM_LEDS);
+        leds[pos] += CHSV(gHue + random8(64), 200, 255);
+
+        FastLED.show();
+        FastLED.delay(1000 / FRAMES_PER_SECOND);
+
+        // do some periodic updates
+        EVERY_N_MILLISECONDS(5)
+        {
+            gHue++; // slowly cycle the "base color" through the rainbow
+        }
+    }
+
+    FastLED.clear();
+    FastLED.delay(2000);
 }
